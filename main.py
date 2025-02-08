76d4f5cb929fca16f802dd16497fbf5bcb10e885
Ron Phillips <mcspidy@gmail.com>
2025-02-08 15:15:57 -0600

    Update

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-import init_django_orm  # noqa: E402
-from django.db.models import QuerySet
+"""
+Your task is to write a get_most_active_customers function that returns first
+and last names along with the sum of money spent by 5 most active customers
+(people who spent the highest amount of money) as a QuerySet of tuples. The
+result should be ordered by the sum_of_spent_money field.
+"""
 
-
+# import init_django_orm
+from django.db.models import QuerySet, Sum
 from db.models import LoyaltyProgramParticipant
 
 
 def get_most_active_customers() -> QuerySet[tuple]:
-    pass
+    return LoyaltyProgramParticipant.objects.values(
+        "first_name", "last_name").annotate(
+        sum_of_spent_money=Sum("spent_money")
+    ).order_by("-sum_of_spent_money")[:5]
