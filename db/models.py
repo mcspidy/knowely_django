8387fabf428d68a95c69ce6170cb046f4b962c45
Ron Phillips <mcspidy@gmail.com>
2025-02-13 07:35:29 -0600

    Solution

--- db/models.py
+++ db/models.py
@@ -1,2 +1,7 @@
-# models.py
 from django.db import models
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    phone_number = models.CharField(max_length=20)
---
+++ main.py
@@ -0,0 +1,11 @@
+import init_django_orm  # noqa: E402
+from django.db.models import QuerySet, Q
+from db.models import Customer
+
+def get_clients_with_i_and_o() -> QuerySet:
+    """
+    returns all the customers whose first name starts with the letter I or
+    last name contains the letter o
+    """
+    return (Customer.objects.filter(Q(first_name__startswith="I")
+                                    | Q(last_name__icontains="o")))
