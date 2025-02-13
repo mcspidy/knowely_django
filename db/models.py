b281edf0deb978934ee74f20053257ced5cf502b
Ron Phillips <mcspidy@gmail.com>
2025-02-13 07:31:37 -0600

    Reset

--- db/models.py
+++ db/models.py
@@ -1,7 +1,1 @@
-from django.db import models
-
-
-class Customer(models.Model):
-    first_name = models.CharField(max_length=64)
-    last_name = models.CharField(max_length=64)
-    phone_number = models.CharField(max_length=20)
+# models.py
