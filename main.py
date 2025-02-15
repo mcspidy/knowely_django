5fa6c10b98387cfbf6dbf68d41e832d87db21411
Ron Phillips <mcspidy@gmail.com>
2025-02-15 06:04:22 -0600

    Solution

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # main.py
+import init_django_orm  # noqa: E402
+from db.models import Chat
+
+
+def get_actual_chats() -> list[Chat]:
+    """
+    Returns a list of up-to-date chats. A chat is active when it contains a
+    message sent later than 2020.
+    """
+    return Chat.objects.filter(message__sent__gt="2020-12-31")
